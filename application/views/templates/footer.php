e49668c0807225a0181981e013a7df79f3231fbd
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-04-07 21:34:01 +0700

    Add footer

---
+++ application/views/templates/footer.php
@@ -0,0 +1,6 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- Fallback kalau CDN gagal atau localhost -->
+<script>window.jQuery || document.write('<script src="js/jquery-1.11.0.min.js">\x3C/script>')</script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
