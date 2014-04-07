4aa57a3fab48d09163cf54dc3dffe0e1a6aa0bac
Adhika <adhikaaryantio.x6@gmail.com>
2014-04-07 17:20:40 +0700

    First Commit

---
+++ application/controllers/site.php
@@ -0,0 +1,13 @@
+<?php 
+
+class Site extends CI_Controller {
+		function index(){
+				echo 'hello world';
+		}
+
+		function dosomething(){
+				echo 'lalala';
+		}
+}
+
+?>
---
+++ application/views/addemployee_view.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+<title>Tambah Karyawan</title>
+
+<?php include 'include_css.php'; ?>
+<link href="../bootstrap/assets/signin.css" rel="stylesheet">
+
+</head>
+ 
+<body>
+<?php include 'header.php'; ?>
+<h2 class="form-signin-heading text-center">Selamat Datang</h2>
+<form class="form-horizontal" role="form">
+  <div class="form-group">
+    <label for="namaKaryawan3" class="col-sm-2 control-label">Nama Karyawan</label>
+    <div class="col-sm-8">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="alamat3" class="col-sm-2 control-label">Alamat</label>
+    <div class="col-sm-8">
+      <textarea class="form-control" rows="3"> </textarea>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="namaKaryawan3" class="col-sm-2 control-label">Tempat Lahir</label>
+    <div class="col-sm-8">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="namaKaryawan3" class="col-sm-2 control-label">Tanggal Lahir (DD/MM/YY)</label>
+    <div class="col-sm-8">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="namaKaryawan3" class="col-sm-2 control-label">Divisi</label>
+    <div class="col-sm-8">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="namaKaryawan3" class="col-sm-2 control-label">Jabatan</label>
+    <div class="col-sm-8">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="namaKaryawan3" class="col-sm-2 control-label">Tanggal Diterima (DD/MM/YY)</label>
+    <div class="col-sm-8">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="namaKaryawan3" class="col-sm-2 control-label">File Input</label>
+    <div class="col-sm-8">
+      <input type="file" id="exampleInputFile">
+      <p class="help-block">Put the photo here.</p>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Submit</button>
+    </div>
+  </div>
+</form>
+</body>
+</html>
