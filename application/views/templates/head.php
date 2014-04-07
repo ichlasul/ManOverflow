c69d095f7fb0385fa73cfe93c3d62c82ca6a523e
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-04-07 22:04:23 +0700

    Update css url

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -4,18 +4,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+  <link rel="shortcut icon" href="<?php echo base_url('bootstrap/ico/favicon.ico'); ?>">
 
   <title>Selamat Datang</title>
 
   <!-- Bootstrap core CSS --> 
-  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url('bootstrap/css/bootstrap.min.css'); ?>"  rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="bootstrap/assets/signin.css" rel="stylesheet">
-
-  <!-- Just for debugging purposes. Don't actually copy this line! -->
-  <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+  <link href="<?php echo base_url('bootstrap/assets/signin.css'); ?>" rel="stylesheet">
 
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
