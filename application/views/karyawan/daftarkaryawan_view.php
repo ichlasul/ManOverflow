85fe3862c1e3f65239cc333e1f7e673f8361dc83
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-04-07 23:03:45 +0700

    Add carikaryawan interface

--- application/views/karyawan/daftarkaryawan_view.php
+++ application/views/karyawan/daftarkaryawan_view.php
@@ -7,18 +7,14 @@
     <?php $this->load->view('templates/header'); ?>
 
     <div class="container">
-      <form class="form-signin" role="form">
-        <h2 class="form-signin-heading text-center">Selamat Datang</h2>
-        <input type="email" class="form-control" placeholder="NIP" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
-
       <div class="col-md-8 col-md-offset-2 jumbotron">
-        asdsads
+        <form class="form" role="form">
+          <div class="form-group col-md-9">
+            <label class="sr-only" for="keyword">Kata Kunci Pencarian</label>
+            <input type="text" class=" form-control input-lg" id="keyword" placeholder="Masukkan Kata Kunci Pencarian">
+          </div>
+          <button type="submit" class="btn btn-primary btn-lg col-md-3">Cari</button>
+        </form>
       </div>
     </div> <!-- /container -->
     <?php $this->load->view('templates/footer'); ?>
