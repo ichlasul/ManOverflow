9745e36b5131337c9c7e36ac93e8e871a04e9625
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-04-07 21:56:29 +0700

    Update header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,13 +8,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="login">Versatile</a>
+      <a class="navbar-brand" href="<?php echo site_url(); ?>">Versatile</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="#">Lihat Karyawan</a></li>
+        <li><a href="<?php echo site_url('karyawan/cari'); ?>">Cari Karyawan</a></li>
         <li><a href="addemployee">Tambah Karyawan</a></li>
         <li><a href="#">Track Record</a></li>
         <li><a href="#">Penjadwalan</a></li>
