638b175d95297985957500d939bef63b13f9407a
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-04-20 00:56:11 +0700

    CariKaryawan now uses panel

--- application/views/karyawan/daftarkaryawan_view.php
+++ application/views/karyawan/daftarkaryawan_view.php
@@ -7,33 +7,42 @@
     <?php $this->load->view('templates/header'); ?>
 
     <div class="container">
-      <div class="col-md-8 col-md-offset-2 jumbotron">
-        <div class="row">
-          <form class="form" action="<?php echo site_url('karyawan/search'); ?>" method="post" role="form">
-            <div class="form-group col-md-9">
-              <label class="sr-only" for="keyword">Kata Kunci Pencarian</label>
-              <input type="text" class=" form-control input-lg" name = "keyword" id="keyword" placeholder="Masukkan Kata Kunci Pencarian">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3>Daftar Karyawan</h3>
+          </div>
+
+          <div class="panel-body">
+            <div class="row">
+              <form class="form" action="<?php echo site_url('karyawan/search'); ?>" method="post" role="form">
+                <div class="form-group col-md-9">
+                  <label class="sr-only" for="keyword">Kata Kunci Pencarian</label>
+                  <input type="text" class="form-control input-lg" name="keyword" id="keyword" placeholder="Masukkan Kata Kunci Pencarian">
+                </div>
+                <div class="col-md-3">
+                  <button type="submit" class="btn btn-primary btn-lg col-md-12">Cari</button>
+                </div>
+              </form>
             </div>
-            <button type="submit" class="btn btn-primary btn-lg col-md-3">Cari</button>
-          </form>
+  
+            <div class="row">
+              <div id="result">
+                <?php if (is_null($result)) { ?>
+                  <div class="alert alert-danger">
+                    Oh tidak, karyawan tidak ditemukan!
+                  </div>
+                <?php } else {
+                          foreach($result as $row){
+                          echo '<p>'. $row->Nama .'</p>';
+
+                  }
+                      } ?>
+              <div>
+            <div> <!-- row -->
+          </div>
         </div>
-
-        <div class="row">
-          <div id="result">
-            <?php if (is_null($result)) { ?>
-              <div class="alert alert-danger">
-                Oh tidak, karyawan tidak ditemukan!
-              </div>
-            <?php } else {
-                      foreach($result as $row){
-                      echo '<p>'. $row->Nama .'</p>';
-
-
-              }
-                  } ?>
-          <div>
-        <div> <!-- row -->
-      </div>  <!-- jumbotron -->
+      </div> 
     </div> <!-- /container -->
     <?php $this->load->view('templates/footer'); ?>
   </body>
