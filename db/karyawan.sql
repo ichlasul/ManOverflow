2efeb71efdc0bbe6b155c5c1107e5fd47405707d
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-04-20 16:43:31 +0700

    Dump without spaces

--- db/karyawan.sql
+++ db/karyawan.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Inang: 127.0.0.1
--- Waktu pembuatan: 19 Apr 2014 pada 23.15
+-- Waktu pembuatan: 20 Apr 2014 pada 11.42
 -- Versi Server: 5.6.14
 -- Versi PHP: 5.5.6
 
@@ -30,11 +30,11 @@ CREATE TABLE IF NOT EXISTS `karyawan` (
   `NIP` int(11) NOT NULL AUTO_INCREMENT,
   `Nama` varchar(255) NOT NULL,
   `Alamat` varchar(255) NOT NULL,
-  `Tempat Lahir` varchar(255) NOT NULL,
-  `Tanggal Lahir` date NOT NULL,
+  `TempatLahir` varchar(255) NOT NULL,
+  `TanggalLahir` date NOT NULL,
   `Divisi` varchar(255) NOT NULL,
   `Jabatan` varchar(255) NOT NULL,
-  `Tanggal Diterima` date NOT NULL,
+  `TanggalDiterima` date NOT NULL,
   `Foto` varchar(255) NOT NULL,
   PRIMARY KEY (`NIP`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13511082 ;
@@ -43,10 +43,12 @@ CREATE TABLE IF NOT EXISTS `karyawan` (
 -- Dumping data untuk tabel `karyawan`
 --
 
-INSERT INTO `karyawan` (`NIP`, `Nama`, `Alamat`, `Tempat Lahir`, `Tanggal Lahir`, `Divisi`, `Jabatan`, `Tanggal Diterima`, `Foto`) VALUES
+INSERT INTO `karyawan` (`NIP`, `Nama`, `Alamat`, `TempatLahir`, `TanggalLahir`, `Divisi`, `Jabatan`, `TanggalDiterima`, `Foto`) VALUES
 (13214, 'ASEP', '', '', '0000-00-00', '', '', '0000-00-00', ''),
 (13215, 'asdsa', ' ', '', '0000-00-00', '', '', '0000-00-00', ''),
 (13511009, '12', 'asdasd', 'sds', '0002-02-01', '', '', '0012-12-11', ''),
+(13511010, 'wwd', 'wdwd ', 'wdw', '0013-03-13', '', '', '0013-12-13', ''),
+(13511011, 'aji', 'sds ', 'sds', '0013-12-13', '0', '0', '0013-12-13', '0'),
 (13511075, 'Ichlasul Amal', ' ', '', '0000-00-00', '', '', '0000-00-00', ''),
 (13511076, '', ' ', '', '0000-00-00', '', '', '0000-00-00', ''),
 (13511077, '', '', '', '0000-00-00', '', '', '0000-00-00', ''),
