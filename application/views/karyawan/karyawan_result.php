4ca149e833eaf5159de452bf09d3f8e6bf7734a2
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-04-20 18:20:06 +0700

    Add modal box

--- application/views/karyawan/karyawan_result.php
+++ application/views/karyawan/karyawan_result.php
@@ -19,8 +19,28 @@ else
 			        <p>
 			        	<a href="<?php echo site_url('karyawan/profil/'. $row->NIP); ?>" class="btn btn-primary" role="button">Lihat Profil</a>
 			        	<a href="<?php echo site_url('karyawan/edit/'. $row->NIP); ?>" class="btn btn-warning" role="button">Edit</a>
-			        	<a href="#" class="text-danger" role="button">Hapus</a>
+			        	<a href="#" class="text-danger" role="button" data-toggle="modal" data-target="#modal-delete-<?php echo $row->NIP; ?>">Hapus</a>
 		        	</p>
+
+		        	<!-- Modal -->
+					<div class="modal fade" id="modal-delete-<?php echo $row->NIP; ?>" tabindex="-1" role="dialog" aria-labelledby="modal-label-<?php echo $row->NIP; ?>" aria-hidden="true">
+					  <div class="modal-dialog modal-sm">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					        <h3 class="modal-title" id="modal-label-<?php echo $row->NIP; ?>">Yakin Menghapus?</h3>
+					      </div>
+					      <div class="modal-body">
+					        <h4><?php echo $row->Nama; ?></h4>
+			       			<h5><?php echo $row->NIP; ?></h5>
+					      </div>
+					      <div class="modal-footer">
+					        <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+					        <button type="button" class="btn btn-danger">Hapus Karyawan</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
 		      	</div>
     		</div>
     	</div>
