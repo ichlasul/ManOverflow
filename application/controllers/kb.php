74c2a68dc55c5ea7adb51a6b4ff673c4f744b9d6
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-04-29 04:00:50 +0700

    Add Pengetahuan controller

---
+++ application/controllers/kb.php
@@ -0,0 +1,33 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Kelas controller Pengetahuan
+ */
+class Kb extends MY_Controller {
+
+	public function __construct()
+	{
+        parent::__construct();
+        //$this->load->model('Karyawan_model');
+    }
+
+    public function _remap($method, $params = array())
+    {
+        if (method_exists(__CLASS__, $method)) {
+            $this->$method($params);
+        } else {
+            redirect('kb/cari');
+        }
+    }
+
+	public function index($param = '')
+	{
+		redirect('kb/cari');
+	}
+
+	public function cari($param = '')
+	{
+		# code...
+	}
+
+}
