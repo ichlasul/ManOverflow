a9ac1fc93b82b47ad5ef20997df59b61e4f53eef
asep-saepudin <ibnu.komarudin@gmail.com>
2014-05-22 21:34:42 +0700

    fsdfsd

--- application/views/templates/navbar_admin.php
+++ application/views/templates/navbar_admin.php
@@ -9,7 +9,7 @@
       <li><a href="<?php echo site_url('karyawan/cari'); ?>">Cari</a></li>
     </ul>
   </li>
-  <!-- <li class="dropdown ">
+  <li class="dropdown ">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Track Record <b class="caret"></b></a>
     <ul class="dropdown-menu">
       <li><a href="<?php echo site_url('trackrecord/tambah'); ?>">Tambah</a></li>
@@ -18,7 +18,7 @@
       <li class="divider"></li>
       <li><a href="<?php echo site_url('trackrecord/cari'); ?>">Cari</a></li>      
     </ul>
-  </li> -->
+  </li>
   <li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Penjadwalan <b class="caret"></b></a>
     <ul class="dropdown-menu">
